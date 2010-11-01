8d81983fbf38e1ca66051454a73c4c9014018aae
MEXAHIK <MEXAHIKYC@yandex.ru>
2010-11-01 19:11:57 +0200

    Authorization username/password

--- views.py
+++ views.py
@@ -7,6 +7,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from studdb.models import Groups, Student
 from django.shortcuts import get_object_or_404, get_list_or_404
 from studdb.models import StudentForm, GroupForm
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib.auth.decorators import permission_required
 
 def group_list(request):
 	groups = Groups.objects.all()
@@ -16,8 +19,9 @@ def group_list(request):
 def group_detail(request, id):
 	group  = get_object_or_404(Groups, id = id)
 	student_list = group.student_set.all()
-	return render_to_response('group_detail.html', {'s_list': student_list, 'group': group})
+	return render_to_response('group_detail.html', {'s_list': student_list, 'group': group}, context_instance = RequestContext(request))
 
+@permission_required('studdb.change_student')
 def edit_student(request, id):
 	student = get_object_or_404(Student, id = id)
 	form = StudentForm(instance = student)
@@ -28,6 +32,7 @@ def edit_student(request, id):
 			return HttpResponseRedirect('/group/')
 	return render_to_response('edit_student.html', {'student_details': form}, context_instance = RequestContext(request))
 
+@permission_required('studdb.change_groups')
 def edit_group(request, id):
 	group = get_object_or_404(Groups, id = id)
 	form = GroupForm(instance = group)
@@ -37,7 +42,8 @@ def edit_group(request, id):
 			form.save()
 			return HttpResponseRedirect ('/group/')
 	return render_to_response('edit_group.html', {'form': form}, context_instance = RequestContext(request))
-	
+
+@permission_required('studdb.add_groups')	
 def add_group(request):
 	form = GroupForm()
 	if request.method == 'POST':
@@ -47,6 +53,7 @@ def add_group(request):
 			return HttpResponseRedirect('/group/')
 	return render_to_response('add_group.html', {'form': form}, context_instance = RequestContext(request))
 
+@permission_required('studdb.add_student')
 def add_student(request):
 	form = StudentForm()
 	if request.method == 'POST':
@@ -56,6 +63,7 @@ def add_student(request):
 			return HttpResponseRedirect('/group/')
 	return render_to_response('add_student.html', {'form': form}, context_instance = RequestContext(request))
 
+@permission_required('studdb.delete_student')
 def delete_student(request, id):
 	show_delete_student = Student.objects.filter(id = id)
 	if request.method == 'POST':
@@ -64,6 +72,7 @@ def delete_student(request, id):
 		return HttpResponseRedirect('/group/')
 	return render_to_response('delete_student.html', {'d_student': show_delete_student}, context_instance = RequestContext(request))
 
+@permission_required('studdb.delete_groups')
 def delete_group(request, id):
 	d_group = get_object_or_404(Groups, id = id)
 	show_delete_group = Groups.objects.filter(id = id)
@@ -73,6 +82,28 @@ def delete_group(request, id):
 		return HttpResponseRedirect('/group/')
 	return render_to_response('delete_group.html', {'d_group': show_delete_group, 'student_list': show_delete_student}, context_instance = RequestContext(request))
 
-def test(request):
-	return render_to_response('test.html')
+def user_detail(request, id):
+	form = get_list_or_404(User, id = id)
+	user = get_object_or_404(User, id = id)
+#	perm = user.has_perms()
+	return render_to_response('user_page.html', {'form': form, 'perm': user}, context_instance = RequestContext(request))
 
+def logout_user(request):
+	auth.logout(request)
+	return HttpResponseRedirect('/group/')
+	
+def login_user(request):
+	if request.method == "GET":
+		username = request.GET.get('username')
+		password = request.GET.get('password')
+		user = auth.authenticate(username = username, password = password)
+		if user is not None and user.is_active:
+			auth.login(request, user)
+			return HttpResponseRedirect('/group/')
+	else:
+		return HttpResponseRedirect('/autch/')
+	return HttpResponseRedirect('/autch/')
+
+def autch(request):
+	return render_to_response ('autch.html')
+	
